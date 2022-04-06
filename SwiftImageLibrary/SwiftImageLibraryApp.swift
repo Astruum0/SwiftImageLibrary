b0d0555212b061499c2cad5091275617035b2c64
AVELLA <arthur.vella@ynov.com>
2022-04-06 10:25:24 +0200

    Initial Commit

---
+++ SwiftImageLibrary/SwiftImageLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftImageLibraryApp.swift
+//  SwiftImageLibrary
+//
+//  Created by Arthur VELLA on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftImageLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
